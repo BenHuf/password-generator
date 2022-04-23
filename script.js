726b8c31082bf6bf56fd021f12961a9d7c10afb2
BenHuf <benthehuffman@gmail.com>
2022-04-23 19:18:06 -0400

    Created develop branch

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment code here
 
+// promptLength function
+var promptLength = function() {
+  // prompt user to input the desired length of the password (8 - 128)
+  var length = parseInt(window.prompt("How many characters should this password have?\n\nPlease enter a number between 8 - 128."));
+  // if the input is invalid alert the user and run the prompt again
+  if (!length || length < 8 || length > 128) {
+    window.alert("Please enter a number between 8 - 128.");
+    return promptLength();
+  }
+  // if the input is valid return the desired password length
+  return length;
+}
 
+var generatePassword = function() {
+  // call each necessary function, store the returned value as a variable, and console.log the variable
+  var length = promptLength();
+  console.log("This password should contain", length, "characters.");
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
