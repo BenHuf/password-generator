1c848f4f91e8d6996c516f48794572fa7ceca1d1
BenHuf <benthehuffman@gmail.com>
2022-04-23 19:41:12 -0400

    Added functions to prompt for desired password length and character types

--- script.js
+++ script.js
@@ -4,19 +4,40 @@
 var promptLength = function() {
   // prompt user to input the desired length of the password (8 - 128)
   var length = parseInt(window.prompt("How many characters should this password have?\n\nPlease enter a number between 8 - 128."));
+  
   // if the input is invalid alert the user and run the prompt again
   if (!length || length < 8 || length > 128) {
     window.alert("Please enter a number between 8 - 128.");
     return promptLength();
   }
+
   // if the input is valid return the desired password length
   return length;
 }
 
+var confirmCharacterTypes = function() {
+  // ask if the password should contain the corresponding character types
+  var confirmLower = window.confirm("Should this password contain LOWERCASE characters?\n(Example -- l o w e r)");
+  var confirmUpper = window.confirm("Should this password contain UPPERCASE characters?\n(Example -- U P P E R)");
+  var confirmNumeric = window.confirm("Should this password contain NUMERIC characters?\n(Example -- 1 2 3 4 5)");
+  var confirmSpecial = window.confirm("Should this password contain SPECIAL characters?\n(Example -- ! @ # $ %)");
+  
+  // if all 4 values are false notify user at least one character type must be used and run through prompts again
+  if (!confirmLower && !confirmUpper && !confirmNumeric && !confirmSpecial) {
+    window.alert("Your password must have at least one of these types of characters.\nPlease select at least one of the 4 character types.");
+    return confirmCharacterTypes();
+  }
+  
+  // if at least one value is true return the results as an array
+  return [confirmLower, confirmUpper, confirmNumeric, confirmSpecial];
+}
+
 var generatePassword = function() {
-  // call each necessary function, store the returned value as a variable, and console.log the variable
+  // call each function, store the returned value as a variable, and console.log the variable and a message
   var length = promptLength();
   console.log("This password should contain", length, "characters.");
+  var characterTypes = confirmCharacterTypes();
+  console.log("This password should contain:\nLowercase characters:", characterTypes[0], "\nUppercase characters:", characterTypes[1], "\nNumeric characters:", characterTypes[2], "\nSpecial characters:", characterTypes[3]);
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
